e9d0f5a6dae1180a5bd0870f55c073e27f19a20a
Marvin Lindner <se24m035@technikum-wien.at>
2025-03-15 15:46:16 +0100

    logger fixes, StreamProcessorApp currently with runtime error, caused by unknown topic "combined-items"

--- kafka-streams-app/src/main/java/com/sfr/streams/StreamProcessorApp.java
+++ kafka-streams-app/src/main/java/com/sfr/streams/StreamProcessorApp.java
@@ -1,10 +1,44 @@
 package com.sfr.streams;
 
-// DOKU: https://kafka.apache.org/documentation/streams/
-public class StreamProcessorApp
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.*;
+import org.apache.kafka.streams.kstream.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class StreamProcessorApp {
+
+    // DOKU: https://kafka.apache.org/documentation/streams/
+    private static final Logger logger = LoggerFactory.getLogger(StreamProcessorApp.class);
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "auto-key-aggregator");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<String, String> inputStream = builder.stream("combined-items");
+
+        KTable<String, Long> countPerKey = inputStream
+                .groupByKey()
+                .count();
+
+        countPerKey.toStream().to("combined-items-counts", Produced.with(Serdes.String(), Serdes.Long()));
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), props);
+
+        streams.start();
+        logger.info("Auto-Key Aggregator running...");
+
+        // Shutdown hook zum sauberen Schließen
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("Shutting down stream...");
+            streams.close();
+        }));
     }
 }
