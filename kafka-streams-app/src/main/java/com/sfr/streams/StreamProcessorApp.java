6cf1732380847f2cd91121db403c4066e0676465
Marvin Lindner <se24m035@technikum-wien.at>
2025-03-15 15:14:42 +0100

    added java project with kafka-streams dependency

---
+++ kafka-streams-app/src/main/java/com/sfr/streams/StreamProcessorApp.java
@@ -0,0 +1,10 @@
+package com.sfr.streams;
+
+// DOKU: https://kafka.apache.org/documentation/streams/
+public class StreamProcessorApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
